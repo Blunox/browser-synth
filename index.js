d85b59baf0c35430e37cc1258f49e3594bb52bef
David MacLeod <davidnmacleod@yahoo.com>
2017-08-22 12:12:38 -0700

    Fixing exports

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ var Synth = require('./src/poly-synth');
 
 module.exports.Synth = Synth.Synth;
 module.exports.setupPlayer = Synth.setupPlayer;
+module.exports.frequencyTable = Synth.frequencyTable;
 
